915953480823cedff280e95bdf64f1c94fbf0f4d
SaharHazan14 <saharhazan5@gmail.com>
2025-12-15 12:28:57 +0200

    removed unnecessary files and comments

--- script.ts
+++ script.ts
@@ -1,46 +1,9 @@
-import { Role } from './generated/prisma/enums'
 import { prisma } from './lib/prisma'
 import { ListRepository } from './src/modules/list/list.repository'
 
 const listRepository = new ListRepository()
 
-async function main() {
-  // create new list
-  /*
-  const list = await listRepository.create("Example List", null, 1)
-  console.log(list)
-  */
-  
-  // Find list by id
-  /*
-  const listFoundById = await listRepository.findById(1)
-  console.log(listFoundById)
-  */
-
-  // Find list by user
-  /*
-  const listFoundByUser1 = await listRepository.findByUser(1)
-  console.log(listFoundByUser1)
-
-  const listFoundByUser2 = await listRepository.findByUser(2)
-  console.log(listFoundByUser2)
-  */
-
-  // Find list by creator
-  /*
-  const listFoundByCreator1 = await listRepository.findByCreator(1)
-  console.log(listFoundByCreator1)
-
-  const listFoundByCreator2 = await listRepository.findByCreator(2)
-  console.log(listFoundByCreator2)
-  */
-
-  // Find user role
-  const role = await listRepository.findUserRole(1, 2)
-  console.log(role)
-
-  /*listRepository.delete(1)*/
-}
+async function main() {}
 
 main()
   .then(async () => {
